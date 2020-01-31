f741d84453c3973468d32ea59e0aa45262f2816f
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-01-31 21:52:30 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # post "/students/new", to: "students#new", as: "new_student"
   # get "/students/new", to: "students#new", as: "student"
-  resources :students, only: [:index, :create,:new]
-  get '/student/:id', to: 'students#show', as: 'student'
+  resources :students, only: [:index, :create,:new,:show]
+  #get '/student/:id', to: 'students#show', as: 'student'
 end
